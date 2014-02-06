b485002849d113525271ec29108093053286a1f3
fimmtiu <dennis@funkplanet.com>
2014-02-05 18:46:29 -0800

    Made a decent set of seed data.

--- app/models/badge.rb
+++ app/models/badge.rb
@@ -1,6 +1,14 @@
 class Badge < ActiveRecord::Base
+  MATERIALS = {1 => "Bronze", 2 => "Silver", 3 => "Gold"}
+
   has_many :users, through: :awards
   has_one :artwork
 
   accepts_nested_attributes_for :artwork
+
+  validates_numericality_of :material, only_integer: true, greater_than: 0, less_than_or_equal_to: MATERIALS.length
+
+  def material_name
+    MATERIALS[material]
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,19 @@
-User.create!(name: 'Balls', email: 'your@mom.com', password: 'ballsballs')
-User.create!(name: 'Balls 2', email: 'your@dad.com', password: 'ballsballs')
-Badge.create!(name: 'Nice hat!', description: "That's a sharp-looking hat.", material: 1)
+["Fred Nozzle", "Rosemary Sousaphone", "Victoria Wobble", "Gerald Fnord"].each do |name|
+  User.create!(name: name, email: "#{name.downcase.sub(/\s+/, '.')}@clio.com", password: "testtest")
+end
+users = User.all
+users.last.update_attributes!(active: false)
+
+Badge.create!(name: "Nice Hat!", description: "That's a sharp-looking hat.", material: 1)
+Badge.create!(name: "Excellent Jib", description: "I like the cut of your jib.", material: 1)
+Badge.create!(name: "Snipe Hunter", description: "Skillfully sought the most snipes.", material: 2)
+badges = Badge.all
+
+Award.create!(awarder: users[0], recipient: users[1], badge: badges[0])
+Award.create!(awarder: users[0], recipient: users[1], badge: badges[0])
+Award.create!(awarder: users[1], recipient: users[0], badge: badges[1])
+Award.create!(awarder: users[2], recipient: users[0], badge: badges[2])
+Award.create!(awarder: users[1], recipient: users[3], badge: badges[0])
+
+Recommendation.create!(awarder: users[0], recipient: users[2], badge: badges[2])
+Recommendation.create!(awarder: users[1], recipient: users[0], badge: badges[2])
