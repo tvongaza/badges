40592dda6ac623e25b03d9ba2fdb9b942e7c3d30
fimmtiu <dennis@funkplanet.com>
2014-02-05 19:12:41 -0800

    Added reasons to seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,11 +9,11 @@ Badge.create!(name: "Excellent Jib", description: "I like the cut of your jib.",
 Badge.create!(name: "Snipe Hunter", description: "Skillfully sought the most snipes.", material: 2)
 badges = Badge.all
 
-Award.create!(awarder: users[0], recipient: users[1], badge: badges[0])
-Award.create!(awarder: users[0], recipient: users[1], badge: badges[0])
-Award.create!(awarder: users[1], recipient: users[0], badge: badges[1])
-Award.create!(awarder: users[2], recipient: users[0], badge: badges[2])
-Award.create!(awarder: users[1], recipient: users[3], badge: badges[0])
+Award.create!(awarder: users[0], recipient: users[1], badge: badges[0], reason: "Mauris elit orci, mattis id dignissim eget, placerat et nisl. Sed blandit libero vel enim facilisis viverra. Etiam vitae volutpat velit. Nulla placerat urna eget dui facilisis congue.")
+Award.create!(awarder: users[0], recipient: users[1], badge: badges[0], reason: "Quisque quis nisl sem.")
+Award.create!(awarder: users[1], recipient: users[0], badge: badges[1], reason: "Nunc id ante sodales, bibendum velit eu, fringilla felis. Morbi quis lobortis tortor, ut ultricies nulla.")
+Award.create!(awarder: users[2], recipient: users[0], badge: badges[2], reason: "Praesent nulla purus, volutpat in iaculis eu, imperdiet vel dolor.")
+Award.create!(awarder: users[1], recipient: users[3], badge: badges[0], reason: "Curabitur pellentesque vitae purus a luctus. Ut at pellentesque nisl, vel dignissim erat. Proin pellentesque tempor neque a molestie.")
 
-Recommendation.create!(awarder: users[0], recipient: users[2], badge: badges[2])
-Recommendation.create!(awarder: users[1], recipient: users[0], badge: badges[2])
+Recommendation.create!(awarder: users[0], recipient: users[2], badge: badges[2], reason: "Balls.")
+Recommendation.create!(awarder: users[1], recipient: users[0], badge: badges[2], reason: "Facilisis turpis.")
