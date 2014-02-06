117847b9f460720080c80defe6b2980d9f9520c9
fimmtiu <dennis@funkplanet.com>
2014-02-05 17:45:14 -0800

    Added very provisional seed data.

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+User.create!(name: 'Balls', email: 'your@mom.com', password: 'ballsballs')
+User.create!(name: 'Balls 2', email: 'your@dad.com', password: 'ballsballs')
+Badge.create!(name: 'Nice hat!', description: "That's a sharp-looking hat.", material: 1)
