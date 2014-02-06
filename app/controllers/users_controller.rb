0f8957b21a89f7b8e0f1548e81584f35f00b1301
fimmtiu <dennis@funkplanet.com>
2014-02-05 17:08:17 -0800

    Renamed users/random_slide.html.erb.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,6 @@ class UsersController < ApplicationController
   def random_slide
     # @user = User.active.order('RAND()').first    only works on MySQL. :-(
     @user = User.active.first
-    render :random_slide, layout: false
+    render partial: 'users/slide', layout: false
   end
 end
