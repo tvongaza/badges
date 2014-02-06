370021a3e4f46b3c720b8a5e3d23cc5940bea316
Hanson Wu <hanson.wu@goclio.com>
2014-02-05 17:06:04 -0800

    Add logic to create or verify user from google, add ap gem, add sign in sign out links

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -2,7 +2,16 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   skip_before_filter :authenticate_user!
 
   def google_oauth2
-    
+    @user = User.find_for_google_oauth2(request.env["omniauth.auth"])
+
+    if @user
+      flash[:notice] = I18n.t "devise.omniauth_callbacks.success", :kind => "Google"
+      sign_in_and_redirect @user, :event => :authentication
+      ""
+    else
+      session["devise.google_data"] = request.env["omniauth.auth"]
+      redirect_to root_path
+    end
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,19 @@ class User < ActiveRecord::Base
   has_many :recommendations, foreign_key: :recipient_id, class_name: Recommendation
   has_many :granted_awards, foreign_key: :awarder_id
   has_many :granted_recommendations, foreign_key: :awarder_id
+
+  def self.find_for_google_oauth2(access_token)
+    return unless access_token["extra"]["raw_info"]["hd"] = "goclio.com"
+    User.where(:email => access_token.info["email"]).first || User.create_user_from_token(access_token.info)
+  end
+
+  def self.create_user_from_token(data)
+    User.create(
+      name: data["name"],
+      email: data["email"],
+      password: Devise.friendly_token[0, 20]
+    )
+  end
+
+
 end
